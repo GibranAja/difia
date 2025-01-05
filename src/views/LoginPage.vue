1e117a61d37d65ad51dac62eb1c0e1bcc94488e0
GibranAja <mgibranarraffi@gmail.com>
2025-01-05 19:08:37 +0700

    style: implement reusable component for auth

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <AuthComponent :isLogin= true />
+</template>
+
+<script setup>
+import AuthComponent from '@/components/AuthComponent.vue'
+</script>
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <AuthComponent :is-login = false />    
+</template>
+
+<script setup>
+import AuthComponent from '@/components/AuthComponent.vue';
+</script>
