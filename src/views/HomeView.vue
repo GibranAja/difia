b00348868bb4d88928b8d3fd81075db58cf36d33
Azk08 <aufa.ziya118@gmail.com>
2025-01-08 13:19:50 +0700

    feat: make navigation bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,10 @@
     <div>
         <h1>Ini Home Public</h1>
     </div>
+    <NavigationBar></NavigationBar>
 </template>
 
 <script setup>
+import NavigationBar from '@/components/NavigationBar.vue';
+
 </script>
