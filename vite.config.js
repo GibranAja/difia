225a6b49740cfaba62ed3b77429cbc973e9a29a2
GibranAja <mgibranarraffi@gmail.com>
2025-02-22 08:34:39 +0700

    fix: fix error import png

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.PNG'],
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
